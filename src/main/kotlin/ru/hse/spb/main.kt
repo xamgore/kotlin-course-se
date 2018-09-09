7445bda3513c58197d069fb56da50afb47b06180
xamgore <xamgore@ya.ru>
2018-09-09 04:18:05 +0300

    Complete homework 1

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,55 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
 
-    return words.joinToString(separator = " ")
+private const val MATCH_FAILED: Char = '.'
+private const val QUESTION_MARK: Char = '?'
+
+private fun matchNonQuestionMark(pair: Pair<Char, Char>): Char {
+    val (x, y) = pair
+
+    return when {
+        x == QUESTION_MARK -> y
+        y == QUESTION_MARK -> x
+        x == y -> x
+        else -> MATCH_FAILED
+    }
+}
+
+private fun fillQuestionMarksFromTheOtherPalindromeSide(str: List<Char>): List<Char> {
+    return str.zip(str.reversed()).map(::matchNonQuestionMark)
+}
+
+private fun fillQuestionMarksFromQueue(str: List<Char>,
+                                       queue: MutableList<Char>,
+                                       default: Char = 'a'): List<Char> {
+    val result = str.toMutableList()
+    val half = (result.size + 1) / 2 - 1
+
+    for (idx in half downTo 0) {
+        if (result[idx] == QUESTION_MARK)
+            result[idx] = if (queue.isEmpty()) default else queue.removeAt(0)
+    }
+
+    return result
+}
+
+fun getTitle(lettersCount: Int, pattern: String): String? {
+    val match = fillQuestionMarksFromTheOtherPalindromeSide(pattern.toList())
+
+    val mustBeUsed = ('a'..'z').take(lettersCount)
+    val leftToUse = (mustBeUsed.toSet() - match).sortedDescending().toMutableList()
+    val bestMatch = fillQuestionMarksFromQueue(match, leftToUse, default = 'a')
+
+    val notAllWereUsed = leftToUse.isNotEmpty()
+    val notAllMatched = MATCH_FAILED in bestMatch
+
+    return if (notAllMatched || notAllWereUsed) null else
+        fillQuestionMarksFromTheOtherPalindromeSide(bestMatch).joinToString("")
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val numOfLetters = readLine()?.toIntOrNull() ?: return
+    val pattern = readLine() ?: return
+
+    println(getTitle(numOfLetters, pattern) ?: "IMPOSSIBLE")
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,36 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
 
-class TestSource {
+class TestTitleGetter {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
-    }
+    fun `test word with the only question mark`() =
+        assertEquals("a", getTitle(1, "?"))
+
+    @Test
+    fun `ok if the first and last letters are same`() =
+        assertEquals("abba", getTitle(2, "a??a"))
+
+    @Test
+    fun `fails if the first and last letters differs`() =
+        assertNull(getTitle(3, "a?c"))
+
+    @Test
+    fun `ok if all letters are used`() =
+        assertEquals("abba", getTitle(2, "????"))
+
+    @Test
+    fun `fails if there are unused letters`() =
+        assertNull(getTitle(3, "????"))
+
+    @Test
+    fun `check that letters are filled by alphabet order`() =
+        assertEquals("abba", getTitle(2, "?b?a"))
+
+    @Test
+    fun `stress test`() =
+        assertEquals("aaaeaaakadavbfghijlomnpqrcstuwxyyxwutscrqpnmoljihgfbvadakaaaeaaa",
+            getTitle(25,
+                "???e???k?d?v?????????????c??????????????????o???????????????????"))
 }
